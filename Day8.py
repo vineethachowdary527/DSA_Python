25ab45502f62b7d04f9451e078e4e2323abd5714
vineethachowdary527 <vineethachowdary527@gmail.com>
2026-06-26 15:24:10 +0530

    Create Day8.py

---
+++ Day8.py
@@ -0,0 +1,23 @@
+#Merge Sort Program
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    mid = len(arr) // 2
+    left = merge_sort(arr[:mid])
+    right = merge_sort(arr[mid:])
+
+    result = []
+    while left and right:
+        if left[0] < right[0]:
+            result.append(left.pop(0))
+        else:
+            result.append(right.pop(0))
+
+    result.extend(left)
+    result.extend(right)
+    return result
+
+numbers = [8, 3, 5, 1, 7]
+
+print("Sorted List:", merge_sort(numbers))
